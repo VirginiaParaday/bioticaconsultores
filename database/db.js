5fc228be498b50c067b403a9cc692b5eb0ceb84b
AiloGibsonBrand <ailogibsonbrand@gmail.com>
2026-03-18 11:39:50 -0500

    shema

--- database/db.js
+++ database/db.js
@@ -11,7 +11,7 @@ const pool = new Pool(
         port: process.env.DB_PORT,
         database: process.env.DB_NAME,
         user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
+        password: String(process.env.DB_PASSWORD),
         ssl: process.env.DB_SSL === 'true',
       }
 );
